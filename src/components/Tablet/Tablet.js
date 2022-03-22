da6e58aa8387a09dc124de6b91c7752cda9f0d21
khadiza21 <bibikhadiza474@gmail.com>
2022-03-22 21:05:42 +0600

    how reacts works

---
+++ src/components/Tablet/Tablet.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Tablet extends Component {
+  //   constructor() {
+  //     //   before 2019 state decleared in the constructor
+  //   }
+  //   componentDidMount() {
+  //     //   and call the stat in the componentDidMount
+  //   }
+  render() {
+    return (
+      <div>
+        <h3>This is tablet.</h3>
+      </div>
+    );
+  }
+}
+
+export default Tablet;
--- src/components/Watch/Watch.js
+++ src/components/Watch/Watch.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Display from "../Display/Display";
+import Tablet from "../Tablet/Tablet";
 
 const Watch = () => {
   const [steps, setSteps] = useState(0);
@@ -12,11 +13,12 @@ const Watch = () => {
     console.log(steps);
   }, [steps]);
   return (
-    <div style={{border:"3px solid green",margin:"25px"}}>
+    <div style={{ border: "3px solid green", margin: "25px" }}>
       <h1>Counting Time</h1>
       <h4>Step {steps}</h4>
       <button onClick={increaseSteps}>Count</button>
       <Display name="addidus" steps={steps}></Display>
+      <Tablet></Tablet>
     </div>
   );
 };
